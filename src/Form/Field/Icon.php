7968b0cdade78f6145298cf708761e2d162978c6
Dobeen <23735841@qq.com>
2025-05-20 14:54:16 +0800

    Icon字段扩展

--- src/Form/Field/Icon.php
+++ src/Form/Field/Icon.php
@@ -6,10 +6,11 @@ class Icon extends Text
 {
     public static $js = '@fontawesome-iconpicker';
     public static $css = '@fontawesome-iconpicker';
+    protected $withoutIconPicker = false;
 
     public function render()
     {
-        $this->addScript();
+        $this->addScript($this->withoutIconPicker);
 
         $this->prepend("<i class='fa {$this->value()}'>&nbsp;</i>")
             ->defaultAttribute('autocomplete', 'off')
@@ -18,8 +19,14 @@ class Icon extends Text
         return parent::render();
     }
 
-    protected function addScript()
+    protected function addScript($withoutIconPicker)
     {
+        $picker = '';
+        if (!$withoutIconPicker) {
+            $picker = <<<JS
+                field.iconpicker({placement:'bottomLeft', animation: false});
+JS;
+        }
         $this->script = <<<JS
 setTimeout(function () {
     var field = $('{$this->getElementClassSelector()}'),
@@ -27,25 +34,31 @@ setTimeout(function () {
         showIcon = function (icon) {
             parent.find('.input-group-prepend .input-group-text').html('<i class="' + icon + '"></i>');
         };
-    
-    field.iconpicker({placement:'bottomLeft', animation: false});
-    
+
+    $picker
+
     parent.find('.iconpicker-item').on('click', function (e) {
        showIcon($(this).find('i').attr('class'));
     });
-    
+
     field.on('keyup', function (e) {
         var val = $(this).val();
-        
+
         if (val.indexOf('fa-') !== -1) {
             if (val.indexOf('fa ') === -1) {
                 val = 'fa ' + val;
             }
         }
-        
+
         showIcon(val);
     })
 }, 1);
 JS;
+    }
+
+    public function withoutIconPicker(): Icon
+    {
+        $this->withoutIconPicker = true;
+        return $this;
     }
 }
